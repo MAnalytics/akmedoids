820f721c457b08745c639f5b47854f0c4a8f0d49
geoMADE <geomad@leeds.ac.uk>
2020-01-06 18:40:57 +0000

    population data

---
+++ R/population.R
@@ -0,0 +1,9 @@
+#' @title sample population (denominator) data
+#' @description simulated denominator data for two consecutive census years
+#' @name population
+#' @docType data
+#' @usage population
+#' @format A matrix
+#' @keywords datasets
+
+NULL
