baa7af05a4b274d99491cf3972e2dcc9eea8a290
geoMADE <geomad@leeds.ac.uk>
2020-01-06 18:41:30 +0000

    traj data

---
+++ R/traj.R
@@ -0,0 +1,9 @@
+#' @title Longitudinal dataset
+#' @description Simulated longitudinal datasets with missing values (\code{NA}, \code{Inf}, \code{null})
+#' @name traj
+#' @docType data
+#' @usage traj
+#' @format A matrix
+#' @keywords datasets
+
+NULL
