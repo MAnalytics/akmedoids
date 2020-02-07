a90dd9b064bfda305e05a04161f8a36700e9947d
geoMADE <geomad@leeds.ac.uk>
2020-02-07 19:35:11 +0000

    update dataimputation.R

--- R/dataImputation.R
+++ R/dataImputation.R
@@ -25,43 +25,43 @@
 #' @return A data.frame with missing values (\code{NA}, \code{Inf}, \code{null}) imputed according to the a specified technique.
 #' @export
 
-dataImputation <- function(traj, id_field = FALSE, method = 2, replace_with = 1, fill_zeros = FALSE){
+dataImputation <- function(traj, id_field = FALSE,
+                           method = 2, replace_with = 1,
+                           fill_zeros = FALSE){
 
-  dat <- as.data.frame(traj)
+dat <- as.data.frame(traj)
 
-  coln_ <- colnames(dat)
+coln_ <- colnames(dat)
 
-  #check if there is unique(id) field
-  if(id_field==TRUE){
-    backup_dat <- dat
-    dat <- dat[,2:ncol(dat)]
-  }
+#check if there is unique(id) field
+if(id_field==TRUE){
+  backup_dat <- dat
+  dat <- dat[,2:ncol(dat)]
+}
 
-  #matrix to backup the 'null' information store via the outlier information [TRUE or FALSE]
-  null_mat <- matrix(FALSE, nrow(dat), ncol(dat))
+#matrix to backup the 'null' information store via the outlier information [TRUE or FALSE]
+null_mat <- matrix(FALSE, nrow(dat), ncol(dat))
 
-  for(a in 1:ncol(dat)){ #a<-1
-    sub_ <- suppressWarnings(as.numeric(as.character(dat[a,])))
-    i_ <- which(is.na(sub_))
-    j_ <- which(is.infinite(sub_))
-    k_ <- which(is.null(sub_))
-    i_m <- unique(c(i_, j_, k_))[order(unique(c(i_, j_, k_)))]
+for(a in seq_len(ncol(dat))){
+  sub_ <- suppressWarnings(as.numeric(as.character(dat[a,])))
+  i_ <- which(is.na(sub_))
+  j_ <- which(is.infinite(sub_))
+  k_ <- which(is.null(sub_))
+  i_m <- unique(c(i_, j_, k_))[order(unique(c(i_, j_, k_)))]
+  null_mat[a,i_m] <- TRUE
+}
 
-    null_mat[a,i_m] <- TRUE
-  }
-
-  # arithmetic method
-  if(method==1){
-    #Use column values in order to determine the new value.
-    if(replace_with==1 | replace_with==2 | replace_with==3){
-      fill_count <- 0
-      for(g in 1:ncol(null_mat)){ #g<-2
-        i_m <- which(null_mat[,g]==TRUE)
-        subset_ <- dat[-i_m,g]
-
-        if(length(i_m)!=0){fill_count <- fill_count + length(i_m)}
-
-        if(length(i_m)!=0){
+#arithmetic method
+if(method==1){
+  #Use column values in order to determine the new value.
+  if(replace_with==1 | replace_with==2 | replace_with==3){
+    fill_count <- 0
+    for(g in seq_len(ncol(null_mat))){ #g<-2
+      i_m <- which(null_mat[,g]==TRUE)
+      subset_ <- dat[-i_m,g]
+      if(length(i_m)!=0){fill_count <-
+        fill_count + length(i_m)}
+      if(length(i_m)!=0){
           if(replace_with==1){
             ave_ <- mean(subset_)
           }
@@ -72,86 +72,82 @@ dataImputation <- function(traj, id_field = FALSE, method = 2, replace_with = 1,
             ave_ <- max(subset_)
           }
           dat[i_m,g] <- ave_
-        }
       }
     }
+  }
 
-    #Use row values in order to determine the new value.
-    if(replace_with==4 | replace_with==5 | replace_with==6){
-      fill_count <- 0
-      for(g in 1:nrow(null_mat)){ #g<-2
-        i_m <- which(null_mat[g,]==TRUE)
-        subset_ <- dat[g, -i_m]
-
-        if(length(i_m)!=0){fill_count <- fill_count + length(i_m)}
-
-        if(length(i_m)!=0){
-          if(replace_with==4){
-            ave_ <- round(mean(as.numeric(as.character(subset_))),digits=2)
-          }
-          if(replace_with==5){
-            ave_ <- min(as.numeric(as.character(subset_)))
-          }
-          if(replace_with==6){
-            ave_ <- max(as.numeric(as.character(subset_)))
-          }
-          dat[g, i_m] <- ave_
-        }
+ #Use row values in order to determine the new value.
+ if(replace_with==4 | replace_with==5 | replace_with==6){
+  fill_count <- 0
+  for(g in seq_len(nrow(null_mat))){ #g<-2
+    i_m <- which(null_mat[g,]==TRUE)
+    subset_ <- dat[g, -i_m]
+    if(length(i_m)!=0){fill_count <-
+      fill_count + length(i_m)}
+    if(length(i_m)!=0){
+      if(replace_with==4){
+        ave_ <- round(mean(as.numeric(as.character(subset_))),digits=2)
       }
+      if(replace_with==5){
+        ave_ <- min(as.numeric(as.character(subset_)))
+      }
+      if(replace_with==6){
+        ave_ <- max(as.numeric(as.character(subset_)))
+      }
+        dat[g, i_m] <- ave_
     }
+  }
+ }
+}
 
+#regression method
+if(method==2){
+#keep count of missing data
+ fill_count <- 0
+ for(k in seq_len(nrow(dat))){ #k<-10
+  y <- suppressWarnings(as.numeric(as.character(dat[k,])))
+  x <- 1:length(y)
+  known <- data.frame(x, y)
+
+  #check which data points are missing,
+  #then remove them from the data point
+  #include zeros
+  if(fill_zeros==FALSE){
+    known_1 <- data.frame(known[is.na(known[,2])|is.infinite(known[,2]),])  #
+    known_2 <- data.frame(known[!is.na(known[,2])&!is.infinite(known[,2]),])
   }
 
-  #regression method
-  if(method==2){
-    #keep count of missing data
-    fill_count <- 0
-    for(k in 1:nrow(dat)){ #k<-10
-      y <- suppressWarnings(as.numeric(as.character(dat[k,])))
-      x <- 1:length(y)
-      known <- data.frame(x, y)
-
-      #check which data points are missing, then remove them from the data point
-      #include zeros
-      if(fill_zeros==FALSE){
-        known_1 <- data.frame(known[is.na(known[,2])|is.infinite(known[,2]),])  #
-        known_2 <- data.frame(known[!is.na(known[,2])&!is.infinite(known[,2]),])
-      }
-
-      if(fill_zeros==TRUE){
-        known_1 <- data.frame(known[is.na(known[,2])|is.infinite(known[,2])|(known[,2]==0),])  #
-        known_2 <- data.frame(known[!is.na(known[,2])&!is.infinite(known[,2])&!(known[,2]==0),])
-      }
-
-      fill_count <- fill_count + nrow(known_1)
-
-      #terminate the programe is there is only one data point
-      if(nrow(known_2)==1){
-        stop("One of the trajectories has only one data point..Unable to interpolate or extrapolate points. Program terminated!!")
-      }
-      #train the available data using linear regression
-      model.lm <- lm(y ~ x, data = known_2)
-      # Use predict the y value for the removed data
-      newY <- predict(model.lm, newdata = data.frame(x = known_1[,1]))
-
-      #add to the original data.
-      dat[k, known_1[,1]] <- round(newY, digits = 2)
-
-    }
-    datF=dat
+  if(fill_zeros==TRUE){
+    known_1 <- data.frame(known[is.na(known[,2])|is.infinite(known[,2])|(known[,2]==0),])  #
+    known_2 <- data.frame(known[!is.na(known[,2])&!is.infinite(known[,2])&!(known[,2]==0),])
   }
 
-  flush.console()
-  print(paste(fill_count, "entries were found/filled!", sep=" "))
-
-  #update the main matrix
-  if(id_field==TRUE){
-    backup_dat[,2:ncol(backup_dat)] <- dat
-    datF <- backup_dat
+  fill_count <- fill_count + nrow(known_1)
+  #terminate the programe is there is only one data point
+  if(nrow(known_2)==1){
+    stop("One of the trajectories has only one data point..Unable to interpolate or extrapolate points. Program terminated!!")
   }
+  #train the available data using linear regression
+  model.lm <- lm(y ~ x, data = known_2)
+  # Use predict the y value for the removed data
+  newY <- predict(model.lm, newdata = data.frame(x = known_1[,1]))
+  #add to the original data.
+  dat[k, known_1[,1]] <- round(newY, digits = 2)
+ }
+  datF=dat
+}
 
-  colnames(datF) <- coln_
-  return(datF)
+flush.console()
+print(paste(fill_count, "entries were found/filled!", sep=" "))
+
+#update the main matrix
+if(id_field==TRUE){
+ backup_dat[,2:ncol(backup_dat)] <- dat
+ datF <- backup_dat
+}
+
+colnames(datF) <- coln_
+return(datF)
 }
 
 
