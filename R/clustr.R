8ebffcd48f8dab05062cf200a0cc8d99570b3ffb
geoMADE <geomad@leeds.ac.uk>
2020-11-14 02:55:08 +0000

    sdfdsfds

--- R/clustr.R
+++
@@ -1,9 +0,0 @@
-#' @title Sample clusters
-#' @description A vector of cluster labels
-#' @name clustr
-#' @docType data
-#' @usage clustr
-#' @format A vector
-#' @keywords datasets
-
-NULL
--- R/population.R
+++
@@ -1,9 +0,0 @@
-#' @title sample population (denominator) data
-#' @description simulated denominator data for two consecutive census years
-#' @name population
-#' @docType data
-#' @usage population
-#' @format A matrix
-#' @keywords datasets
-
-NULL
