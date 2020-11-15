d433b11e0dffe301eb6ceafbebe9488a7057e9fb
geoMADE <geomad@leeds.ac.uk>
2020-11-15 16:46:28 +0000

    delete data R code from main R directory

--- R/popl.R
+++
@@ -1,9 +0,0 @@
-#' @title sample population (denominator) data
-#' @description simulated denominator data for two consecutive census years
-#' @name popl
-#' @docType data
-#' @usage popl
-#' @format A matrix
-#' @keywords datasets
-
-NULL
--- R/traj_wSpaces.R
+++
@@ -1,9 +0,0 @@
-#' @title Datasets with white spaces
-#' @description Trajectories containing white spaces
-#' @name traj_wSpaces
-#' @docType data
-#' @usage traj_wSpaces
-#' @format A dataframe
-#' @keywords datasets
-
-NULL
