b8c494dd82dd23971d532cf05be95ac65e0a5afa
geoMADE <geomad@leeds.ac.uk>
2020-11-15 00:10:01 +0000

    edit population data's name

---
+++ R/popl.R
@@ -0,0 +1,9 @@
+#' @title sample population (denominator) data
+#' @description simulated denominator data for two consecutive census years
+#' @name popl
+#' @docType data
+#' @usage popl
+#' @format A matrix
+#' @keywords datasets
+
+NULL
